79f046ddee2c7899a33effd07739c6d2f54b4759
Sandu-Sezer <162442419+Sandu-Sezer@users.noreply.github.com>
2024-03-18 22:40:38 +0200

    Update 1-1.c

--- 1-1.c
+++ 1-1.c
@@ -5,7 +5,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int* result = (int*) malloc(numsSize * sizeof(int));
     int counter = 0;
 
-    for (int i = 0; i < numsSize; i++){
+    for (int i = 0; i < numsSize - 1; i++){
         for (int j = i + 1; j < numsSize; j++){
             if (nums[i] + nums[j] == target){
                 result[counter++] = i;
