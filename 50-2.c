18e60824bd6bc1dfd740916e78702f18f4d62550
Sandu-Sezer <162442419+Sandu-Sezer@users.noreply.github.com>
2024-03-21 21:01:09 +0200

    Create 50-2.c

---
+++ 50-2.c
@@ -0,0 +1,26 @@
+double myPow(double x, int n) {
+    if (n == 0){
+        return 1;
+    }
+    
+    if (n < 0 ){
+
+        n = abs(n);
+        x = 1 / x;
+
+        if (n % 2 == 0){
+            return myPow(x, n / 2) * myPow(x, n / 2);
+        } else {
+            return x * myPow(x, n / 2) * myPow(x, n / 2);
+        }
+
+    } else {
+
+        if (n % 2 == 0){
+            return myPow(x, n / 2) * myPow(x, n / 2);
+        } else {
+            return x * myPow(x, n / 2) * myPow(x, n / 2);
+        }
+
+    }
+}
